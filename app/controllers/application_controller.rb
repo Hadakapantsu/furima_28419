6fbb9c96fe9190e6ace5568b588f637b31e5b7aa
Hadakapantsu <sakuratsuduki@gmail.com>
2020-08-12 12:05:24 +0900

    gem rails_12factorの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
-end
+  before_action :basic_auth
+  
+    private
+  
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+        username == 'Hadakapantsu' && password == 'KazuSaku1227'  # 環境変数を読み込む記述に変更
+      end
+    end
+  end
