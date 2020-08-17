7afbe7d0b8389e9afab52dfae3267082fa5f23f7
Hadakapantsu <sakuratsuduki@gmail.com>
2020-08-17 14:54:13 +0900

    ユーザー情報表示の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,18 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!  #ログインしているユーザーのみしかアクセスを許可しないようにする  
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :move_to_index, except: [:index, :show] #before_actionによりmove_to_indexメソッドが先に実行され、indexアクションのindex.html.erbページにリダイレクトする
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nichname, :email, :password, :familyname, :fiestname, :rubi_familyname, :rubi_fiestname, :birthday])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
   
