e8d9799c9d9fdd94907045abd037bf6c18ce6e5e
Hadakapantsu <sakuratsuduki@gmail.com>
2020-08-31 15:40:45 +0900

    Rubocop2回目実行・修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path #(id: item_path)
+      redirect_to root_path # (id: item_path)
     else
       render :new
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,15 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category  #モデル名
+  belongs_to_active_hash :category # モデル名
   belongs_to_active_hash :condition
   belongs_to_active_hash :cost_burden
   belongs_to_active_hash :shipping_place
   belongs_to_active_hash :shipping_day
- 
+
   belongs_to :user
   has_one_attached :image
-  
-  with_options presence: true do #カラム名
+
+  with_options presence: true do # カラム名
     validates :name, presence: true
     validates :text, presence: true
     validates :category_id, presence: true
